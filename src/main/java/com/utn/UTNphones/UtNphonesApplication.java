db9ce1757af79a52353b8238baae2d9190d36527
MateuGit <facumatiu@hotmail.com>
2020-04-25 16:53:42 -0300

    Conf

--- src/main/java/com/utn/UTNphones/UtNphonesApplication.java
+++ src/main/java/com/utn/UTNphones/UtNphonesApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UtNphonesApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(UtNphonesApplication.class, args);
 	}
 
