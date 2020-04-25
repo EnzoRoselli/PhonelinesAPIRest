ca0796c70899eab9a0eeb2a5d1e88004b25b444c
MateuGit <facumatiu@hotmail.com>
2020-04-25 16:43:04 -0300

    Configuraciones

---
+++ src/main/java/com/utn/UTNphones/UtNphonesApplication.java
@@ -0,0 +1,13 @@
+package com.utn.UTNphones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtNphonesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtNphonesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utn/UTNphones/UtNphonesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utn.UTNphones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtNphonesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
