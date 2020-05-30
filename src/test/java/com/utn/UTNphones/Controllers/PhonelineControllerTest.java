f25d755597616c7ff21db6357dd7491b598e12ad
Enzo Roselli <eroselli@emmsa.net>
2020-05-30 09:03:39 -0300

    Update PhonelineControllerTest.java

--- src/test/java/com/utn/UTNphones/Controllers/PhonelineControllerTest.java
+++ src/test/java/com/utn/UTNphones/Controllers/PhonelineControllerTest.java
@@ -1,12 +1,19 @@
 package com.utn.UTNphones.Controllers;
 
-import com.utn.UTNphones.Domain.Phoneline;
+
+import com.utn.UTNphones.Domains.City;
+import com.utn.UTNphones.Domains.Phoneline;
+import com.utn.UTNphones.Domains.User;
+import com.utn.UTNphones.Exceptions.PhonelineExceptions.PhonelineDoesntExist;
 import com.utn.UTNphones.Services.CityService;
 import com.utn.UTNphones.Services.PhonelineService;
 import com.utn.UTNphones.Services.UserService;
 import org.junit.Before;
+import org.junit.Test;
 import org.mockito.Mock;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 public class PhonelineControllerTest {
@@ -23,5 +30,14 @@ public class PhonelineControllerTest {
         phonelineController = new PhonelineController(phonelineService, cityService);
     }
 
+    @Test
+    public void getByIdOk() throws PhonelineDoesntExist {
+        Phoneline aux = Phoneline.builder().id(1).number("1111111").type("mobile").status(true)
+                .user(new User()).city(new City()).build();
 
+        when(phonelineService.getById(1)).thenReturn(aux);
+        Phoneline returnedPhoneline = phonelineController.getById(1);
+
+        assertEquals(aux, returnedPhoneline);
+    }
 }
